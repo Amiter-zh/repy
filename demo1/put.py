31e728579f578ddcb57635202b7fc0674f8db039
Amiter-zh <15622141125@163.com>
2023-05-27 22:34:30 +0800

    'dome1'

---
+++ demo1/put.py
@@ -0,0 +1,11 @@
+name = input()
+print(name)
+
+# input 使用
+# name1 = input("提示文本1")
+# print(name1)
+
+
+name = input("请输入账号")
+password = input("请输入密码")
+print(name,password,sep=':')
---
+++ demo1/var.py
@@ -0,0 +1,29 @@
+# 相比于// # ctrl+/
+print(518)
+# print(唐三)
+# NameError: name '唐三' is not defined 计算器无法编译str 故加‘’
+print('唐三','低功耗')
+# 逗号间隔
+# ctrl + shift + k 输出改行
+# alt + 方向键 移动 
+# alt + shift +方向键 向...复制
+print('唐三', '低功耗','dawdaw',sep='--')
+# sep 分隔符中间所显示的符号
+print('唐三','低功耗',end ="--")
+print('唐三','低功耗',end ="\n")
+# end末尾添加符号 默认会出现一个换行符号
+
+# 變量内存标志
+
+name = 'kfv'
+print(name) 
+temp=name
+name = 'vw50' 
+print(name) 
+print(temp) 
+# 明明规范
+# 1.最好有一定意义
+# 数字字母 下划线 不能以数字开头 类java
+# = 读作复制 叫做赋值符号
+
+# 
